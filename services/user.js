034f96817d09c575f0e64d4258e998d700a01941
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-06-12 19:15:32 +0300

    Implement JWT signing on registration.

--- services/user.js
+++ services/user.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+
+const JWT_SECRET = 's1231fasasdae23fdaasad22e';
 
 async function register(email, password) {
     const existing = await User.findOne({ email: new RegExp(`^${email}$`, 'i') });
@@ -13,11 +16,24 @@ async function register(email, password) {
         hashedPassword: await bcrypt.hash(password, 10)
     });
     await user.save();
-    return user;
+    return createSession(user);
 };
 async function login(email, password) {
 
 };
+
+function createSession(user) {
+    return {
+        email: user.email,
+        _id: user._id,
+        accessToken: jwt.sign({
+            email: user.email,
+            _id: user._id
+        }, JWT_SECRET)
+    };
+};
+
+
 module.exports = {
     register,
     login
