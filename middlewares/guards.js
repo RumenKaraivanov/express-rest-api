bd87fe1063b2aca135bf39f13a7471eb2a131735
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-06-20 18:38:47 +0300

    added guard middlewares

---
+++ middlewares/guards.js
@@ -0,0 +1,32 @@
+function isUser() {
+    return (req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.status(401).json({ message: 'Unauthorized. Please log in.' });
+        };
+    };
+};
+function isGuest() {
+    return (req, res, next) => {
+        if (!req.user) {
+            next();
+        } else {
+            res.status(400).json({ message: 'You are already logged in.' });
+        };
+    };
+};
+function isOwner() {
+    return (req, res, next) => {
+        if (req.user && req.user._id == req.locals.item._ownerId) {
+            next();
+        } else {
+            res.status(403).json({ message: 'You cannot modify this record.' });
+        };
+    };
+};
+module.exports = {
+    isUser,
+    isGuest,
+    isOwner
+};
