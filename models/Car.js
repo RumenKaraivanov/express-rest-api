d9ae47749684bd3f1d5e604b044003d03d566573
RumenKaraivanov <rumbata_89.1989@abv.bg>
2022-07-03 12:47:44 +0300

    Added valid URL validation

--- models/Car.js
+++ models/Car.js
@@ -1,10 +1,18 @@
 const { Schema, model } = require('mongoose');
+const URL_PATTERN = /^https?:/;
 
 const schema = new Schema({
     model: { type: String, required: [true, 'Model is required!'] },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    imageUrl: { type: String, required: true },
+    description: { type: String, required: [true, 'Description is required!'] },
+    price: { type: Number, required: [true, 'Price is required!'] },
+    imageUrl: {
+        type: String, required: true, validate: {
+            validator(value) {
+                return URL_PATTERN.test(value);
+            },
+            message: 'The image must be valid URL!'
+        }
+    },
     _ownerId: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
